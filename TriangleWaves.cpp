b806dde3ced25696b49fcc329f756db87b3e841c
Peixe <peixe@libra.soho>
2020-05-30 15:26:36 -0300

    Initial commit

---
+++ TriangleWaves.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <iostream>
+#include <cmath>
+using namespace std;
+void Gerador();
+int main() {
+    cout << "Bem vindo ao TriangleWaves!" << endl;
+
+    Gerador();
+}
+
+void Gerador() {
+
+    
+}
