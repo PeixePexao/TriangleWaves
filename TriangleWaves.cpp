45ed00796b54dd3a1d852b61b9e13f779437ebf2
Peixe <peixe@libra.soho>
2020-05-30 15:50:32 -0300

    15:50 30/5/2020 FUNCIONAL

--- TriangleWaves.cpp
+++ TriangleWaves.cpp
@@ -2,14 +2,41 @@
 #include <iostream>
 #include <cmath>
 using namespace std;
-void Gerador();
+void Generator();
 int main() {
-    cout << "Bem vindo ao TriangleWaves!" << endl;
+    cout << "Bem vindo ao TriangleWaves" << endl;
 
-    Gerador();
+    Generator();
 }
 
-void Gerador() {
+void Generator() {
 
-    
+    cout << "Quantas vezes devemos aumentar o x em x² + (x-1)²? Lembrando que x começará como 4: ";
+    int vezes, contador;
+    cin >> vezes;
+    contador = 4;
+    double somas[vezes];
+    double quadrados[vezes * 2];
+    if (cin.fail()) {
+        cout << "Invalid input" << endl;
+        Generator();
+    }
+
+    for (int i = 0; i < vezes; i++) {
+        somas[i] = pow(contador, 2) + pow(contador - 1, 2);
+        contador++;
+    }
+    for (int i = 0; i < (vezes * 2); i++) {
+        quadrados[i] = pow(i, 2);
+    }
+    cout << "Initiating checks..." << endl; 
+    for (int i = 0; i < vezes; i++) {
+        for (int z = 0; z < vezes * 2; z++) {
+            if (somas[i] == quadrados[z]) {
+                cout << "Match: " << somas[i] << " é igual a " << quadrados[z];
+            }
+        }
+        
+    }
+
 }
